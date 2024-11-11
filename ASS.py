e300b757897f3427ecf1bd0aa857faa8be9fd1c9
Darkou <34028450+Darkou@users.noreply.github.com>
2024-11-11 13:03:05 +0100

    v0.3
    optimization and new funcs

--- ASS.py
+++ ASS.py
@@ -34,7 +34,7 @@ def Where():
     r = input(f"{Fore.GREEN}[1] All time\n[2] Year\n[3] Month \n[4] Week \n> {Style.RESET_ALL}")
 
     try:
-        r = int(r)
+        r = int(r[0])
     except:
         pystyle.System.Clear()
         Page()
@@ -48,7 +48,10 @@ def Where():
         Stats(2629743, "MONTH") # 1 month in epoch
     elif r == 4:
         Stats(604800, "WEEK") # 1 week in epoch
-    else:pass
+    else:
+        pystyle.System.Clear()
+        Page()
+        Where()
 
 def Stats(range, type):
     pystyle.System.Clear()
@@ -57,26 +60,21 @@ def Stats(range, type):
     total_streams = 0
     total_unique_tracks = {}
     total_artists = {}
-    first_time = False
+    first = False
 
     for song in history:
         song_name = song["master_metadata_track_name"]
         song_artist = song["master_metadata_album_artist_name"]
         song_played_time = song["ms_played"]
         
-        song_date = re.split(r":|-| |T", song["ts"]) # set song date in list [year, month, day, hour, min, s]  
-        song_date[-1] = song_date[-1][0:2] # delete junk from second
-        song_date = [int(i) for i in song_date] # convert to int each el
-        song_date = datetime.datetime(song_date[0], song_date[1], song_date[2], song_date[3], song_date[4], song_date[5]).timestamp()
+        song_date = int(song["offline_timestamp"]/1000) if len(str(song["offline_timestamp"])) > 10 else song["offline_timestamp"]
 
         if song_date >= int(time.time())-range:
-            if not first_time: 
-                first_date = re.split(r":|-| |T", song["ts"])[0:3] # set song date but only year, month, day
-                first_date_full = re.split(r":|-| |T", song["ts"]) # set song date in list [year, month, day, hour, min, s]
-                first_date_full[-1] = first_date_full[-1][0:2] # delete junk from second
-                first_date_full = [int(i) for i in first_date_full] # convert to int each el
-                first_date_epoch = datetime.datetime(first_date_full[0], first_date_full[1], first_date_full[2], first_date_full[3], first_date_full[4], first_date_full[5]).timestamp()
-                first_time = True
+            if not first: # get variables of first date taken into account
+                first_date = int(song["offline_timestamp"]/1000) if len(str(song["offline_timestamp"])) > 10 else song["offline_timestamp"]
+                first_date_time = datetime.datetime.fromtimestamp(first_date).timetuple()
+                
+                first = True
 
             total_streams += 1
             total_time += song_played_time
@@ -93,11 +91,9 @@ def Stats(range, type):
                 total_artists[song_artist][0] = int(total_artists[song_artist][0]) + 1
                 total_artists[song_artist][1] = int(total_artists[song_artist][1]) + song_played_time
     
-    last_date = re.split(r":|-| |T", song["ts"])[0:3] # set song date but only year, month, day
-    last_date_full = re.split(r":|-| |T", song["ts"]) # set song date in list [year, month, day, hour, min, s]
-    last_date_full[-1] = last_date_full[-1][0:2] # delete junk from second
-    last_date_full = [int(i) for i in last_date_full] # convert to int each el
-    last_date_epoch = datetime.datetime(last_date_full[0], last_date_full[1], last_date_full[2], last_date_full[3], last_date_full[4], last_date_full[5]).timestamp()
+    # get variables of last date taken into account
+    last_date = int(song["offline_timestamp"]/1000) if len(str(song["offline_timestamp"])) > 10 else song["offline_timestamp"]
+    last_date_time = datetime.datetime.fromtimestamp(last_date).timetuple()
 
     #set up for display
     total_time = total_time/1000 # convert ms to s
@@ -110,26 +106,26 @@ def Stats(range, type):
     #sort dict by streamed time
     total_unique_tracks = dict(sorted(total_unique_tracks.items(), reverse=True, key=lambda item: item[1][1])) 
     total_artists = dict(sorted(total_artists.items(), reverse=True, key=lambda item: item[1][1]))
-
+    
     #display init
     try:
-        display_date_window = f"Data from {first_date[2]} {list(calendar.month_name)[int(first_date[1])]} {(first_date[0])} to {last_date[2]} {list(calendar.month_name)[int(last_date[1])]} {(last_date[0])}."
-        display_total_time = f"Total time streamed  :" + f"\n> {(d*24+h)*60+min} minutes \n> {d*24+h} hours \n> {d} days, {h} hours, {min} minutes, {s} seconds. "
-        display_average_time = f"Average daily time : {int(((d*24+h)*60+min)/((last_date_epoch-first_date_epoch)/86400))} minutes/day"
-        display_total_streams = f"Total streams : {total_streams}"
-        display_total_unique_tracks = f"Total unique tracks : {len(total_unique_tracks)} ({int(len(total_unique_tracks)/total_streams*100)}%)"
-        display_total_artists = f"Total artists : {len(total_artists)}"
+        display_date_window = f"Data from {first_date_time[2]} {list(calendar.month_name)[int(first_date_time[1])]} {(first_date_time[0])} to {last_date_time[2]} {list(calendar.month_name)[int(last_date_time[1])]} {(last_date_time[0])}."
+        display_total_time = f"Total time streamed  :" + f"\n> {(d*24+h)*60+min} minutes \n> {d*24+h} hours \n> {d} days"
+        display_average_time = f"Average daily time : \n> {int(((d*24+h)*60+min)/((last_date-first_date)/86400))} minutes/day \n> {int(total_streams/((last_date-first_date)/86400))} streams/day"
+        display_total_streams = f"Streams : {total_streams}"
+        display_total_unique_tracks = f"Different tracks : {len(total_unique_tracks)} ({int(len(total_unique_tracks)/total_streams*100)}%)"
+        display_total_artists = f"Different artists : {len(total_artists)}"
         display_top_tracks = "Top tracks :"
         display_top_artists = "Top artists :"
     except UnboundLocalError:
         print(Colorate.Color(Colors.red, "There is not enough data to display that."))
-        time.sleep(3)
+        time.sleep(2)
         pystyle.System.Clear()
         Page()
         Where()
     except Exception as e:
         print(Colorate.Color(Colors.red, f"An error may have occured. ({e})"))
-        time.sleep(3)
+        time.sleep(2)
         pystyle.System.Clear()
         Page()
         Where()
@@ -149,7 +145,7 @@ def Stats(range, type):
     while i < 10:
         track_name = list(total_unique_tracks.keys())[i]
         if i+1 == 10: 
-            display_top_tracks += f"\n#{i+1} - {track_name} " +f"({int(total_unique_tracks[track_name][1]/1000//60)} minutes)"
+            display_top_tracks += f"\n#{i+1} - {track_name} " + f"({int(total_unique_tracks[track_name][1]/1000//60)} minutes)"
         else:
             display_top_tracks += f"\n#{i+1}  - {track_name} " + f"({int(total_unique_tracks[track_name][1]/1000//60)} minutes)"
         i += 1
